57e4e20e429e25a30df214c31bf0bc19e925125c
André Lötzsch <andre.loetzsch@live.de>
2022-02-13 10:23:45 +0100

    Solution JsonFileSink created. LoggerSinkProvider performance improved.

--- Tentakel.Extensions.Logging/src/Tentakel.Extensions.Logging/BackgroundWorker.cs
+++ Tentakel.Extensions.Logging/src/Tentakel.Extensions.Logging/BackgroundWorker.cs
@@ -1,132 +1,61 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using Microsoft.Extensions.Logging;
+using Tentakel.Extensions.Logging.Providers;
 
 namespace Tentakel.Extensions.Logging
 {
     internal class BackgroundWorker : IDisposable
     {
-        private readonly ILogger _innerLogger;
+        private LoggerSinkProvider _provider;
+        private readonly ILogger _logger;
 
-        private readonly List<BackgroundAction> _backgroundActions;
+        private Thread BackgroundThread;
+
+        private readonly List<LogEntry> _logEntryCache = new();
+        private readonly List<LogEntry> _logEntryBackgroundStack = new();
+
         private readonly ManualResetEvent _wait;
-        private readonly List<AutoResetEvent> _waitHandles = new();
 
-        private string _currentActionTrace;
-        private int _disposedBackgroundActions;
+        private bool _logEntryBackgroundStackIsEmpty = true;
 
-        private class BackgroundAction
+        public BackgroundWorker(LoggerSinkProvider provider)
         {
-            internal static long InstanceCounter;
-            private readonly long _instanceCounter;
-
-            public BackgroundAction(Action action, Func<bool> isActive)
-            {
-                Interlocked.Decrement(ref InstanceCounter);
-                this._instanceCounter = InstanceCounter;
-                this.Action = action;
-                this.IsActive = isActive;
-            }
-
-            public readonly Action Action;
-            public readonly Func<bool> IsActive;
-
-            public override string ToString()
-            {
-                return $"{this.Action.Target}.{this.Action.Method} {this._instanceCounter}";
-            }
-        }
-
-        public BackgroundWorker(ILogger innerLogger)
-        {
-            this._innerLogger = innerLogger;
-            this._backgroundActions = new List<BackgroundAction>();
+            this._provider = provider;
+            this._logger = provider.CreateLogger("Tentakel.Logger");
             this._wait = new ManualResetEvent(false);
         }
 
-        public Thread BackgroundThread { get; private set; }
         public bool IsRunning { get; private set; }
 
-        public void AddBackgroundAction(Action action, Func<bool> isActive = null)
+        public int UndoneLogs
+        {
+            get
+            {
+                lock (this._logEntryCache)
+                {
+                    return this._logEntryCache.Count + this._logEntryBackgroundStack.Count;
+                }
+            }
+        }
+
+
+
+        public void AddLogEntry(LogEntry logEntry)
         {
             if (this.IsDisposed) return;
-            if (action == null) throw new ArgumentNullException(nameof(action));
+            if (logEntry == null) return;
 
-            lock (this._backgroundActions)
+            lock (this._logEntryCache)
             {
-                if (isActive == null) isActive = () => true;
-                this._backgroundActions.Add(new BackgroundAction(action, isActive));
+                this._logEntryCache.Add(logEntry);
+                if (!this._logEntryBackgroundStackIsEmpty) return;
+                this._wait.Reset();
                 this._wait.Set();
             }
-        }
-
-        public void InsertBackgroundAction(int index, Action action, Func<bool> isActive = null)
-        {
-            if (this.IsDisposed) return;
-            if (action == null) throw new ArgumentNullException(nameof(action));
-
-            lock (this._backgroundActions)
-            {
-                if (this.IsDisposed) return;
-                if (isActive == null) isActive = () => true;
-                this._backgroundActions.Insert(index, new BackgroundAction(action, isActive));
-            }
-
-            this._wait.Set();
-        }
-
-        public bool WaitOne(int millisecondsTimeout)
-        {
-            return this.WaitOne(TimeSpan.FromMilliseconds(millisecondsTimeout));
-        }
-
-        public bool WaitOne(TimeSpan timeout)
-        {
-            if (this.IsDisposed) return this._disposedBackgroundActions == 0;
-
-            if (this.BackgroundThread == Thread.CurrentThread)
-            {
-                throw new InvalidOperationException(SR.PossibleDeadlockDetected);
-            }
-
-            var wh = new AutoResetEvent(false);
-            var wh1 = wh;
-            var wh2 = wh;
-
-            lock (this._waitHandles)
-            {
-                this._waitHandles.Add(wh);
-            }
-
-            this.AddBackgroundAction(() => wh1.Set(), () =>
-            {
-                lock (this._waitHandles)
-                {
-                    return this._waitHandles.Contains(wh2);
-                }
-            });
-
-            var result = wh.WaitOne(timeout);
-
-            lock (this._waitHandles)
-            {
-                this._waitHandles.Remove(wh);
-            }
-
-            wh.Dispose();
-
-            if (result) return true;
-
-            if (this._innerLogger.IsEnabled(LogLevel.Debug))
-            {
-                this._innerLogger.LogDebug("WaitOne failed! Current action: {StackTrace}", this.StackTrace());
-            }
-
-            if (this.IsRunning) return false;
-            throw new InvalidOperationException(SR.BackgroundWorkerIsNotRunning);
         }
 
         public void Start()
@@ -143,82 +72,83 @@ namespace Tentakel.Extensions.Logging
 
             this.BackgroundThread.Start();
         }
-
         public void Stop()
         {
             if (this.IsDisposed) return;
             this.IsRunning = false;
             this._wait.Set();
         }
-
-        public string StackTrace()
-        {
-            lock (this._backgroundActions)
-            {
-                var sb = new StringBuilder();
-                if (!string.IsNullOrEmpty(this._currentActionTrace))
-                {
-                    sb.AppendLine($"{this._currentActionTrace}*");
-                }
-
-                foreach (var action in this._backgroundActions)
-                {
-                    sb.AppendLine(action.ToString());
-                }
-
-                return sb.ToString();
-            }
-        }
-
         private void Run()
         {
             if (this.IsRunning) return;
             this.IsRunning = true;
 
+            this._wait.WaitOne();
+
             while (this.IsRunning)
             {
-                var nextAction = this.GetNextAction();
+                var next = this.GetNextLogEntry();
 
-                if (nextAction != null)
+                if (next != null)
                 {
                     try
                     {
-                        nextAction.Action();
+                        this._provider.InternalLog(next);
                     }
                     catch (Exception ex)
                     {
-                        this._innerLogger.LogError(ex, "Error occurred during execute background action: {nextAction}!", nextAction);
+                        this._logger.LogError(ex, "Error occurred during execute internal log: {next}!", next);
                     }
                 }
                 else
                 {
+                    lock (this._logEntryCache)
+                    {
+                        this._logEntryBackgroundStackIsEmpty = true;
+                        this._provider.BackgroundStackIsEmpty();
+                    }
+
                     this._wait.Reset();
                     this._wait.WaitOne();
+
+                    Thread.Sleep(10);
+
+                    lock (this._logEntryCache)
+                    {
+                        this._logEntryBackgroundStackIsEmpty = false;
+                    }
                 }
             }
         }
 
-        private BackgroundAction GetNextAction()
+        private int _index;
+
+        private LogEntry GetNextLogEntry()
         {
-            lock (this._backgroundActions)
+            this._index++;
+
+            if (this._index >=  this._logEntryBackgroundStack.Count)
             {
-                BackgroundAction nextAction;
+                this._index = 0;
 
-                do
+                lock (this._logEntryCache)
                 {
-                    nextAction = this._backgroundActions.FirstOrDefault();
-                    if (nextAction != null) this._backgroundActions.Remove(nextAction);
+                    Debug.WriteLine($"_logEntryCache.Count: {_logEntryCache.Count}");
 
-                } while (nextAction != null && !nextAction.IsActive());
+                    this._logEntryBackgroundStack.Clear();
+                    this._logEntryBackgroundStack.AddRange(this._logEntryCache);
+                    this._logEntryCache.Clear();
 
-                this._currentActionTrace ??= string.Empty;
+                    if (this._logEntryBackgroundStack.Count == 0) return null;
 
-                this._currentActionTrace = nextAction == null ?
-                    string.Concat(this._currentActionTrace, " (null)") :
-                    nextAction.ToString();
-
-                return nextAction;
+                }
             }
+
+            //next = this._logEntryBackgroundStack.FirstOrDefault();
+            //if (next != null) this._logEntryBackgroundStack.Remove(next);
+
+            return this._logEntryBackgroundStack[this._index];
+
         }
 
         #region IDisposable
@@ -240,15 +170,15 @@ namespace Tentakel.Extensions.Logging
             this.Stop();
             this.IsDisposed = true;
 
-            this._disposedBackgroundActions = this._backgroundActions.Count;
-            this._backgroundActions.Clear();
+            this._logEntryCache.Clear();
+            this._logEntryBackgroundStack.Clear();
 
             if (this._wait?.SafeWaitHandle != null && !this._wait.SafeWaitHandle.IsClosed)
             {
                 this._wait.Dispose();
             }
 
-            BackgroundAction.InstanceCounter = 0;
+
             if (disposing) GC.SuppressFinalize(this);
         }
 
---
+++ tests/Tentakel.Extensions.Logging.JsonFile/Tentakel.Extensions.Logging.JsonFile.Tests/Class1.cs
@@ -0,0 +1,46 @@
+﻿using Tentakel.Extensions.Logging.Providers;
+using Xunit;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Tentakel.Extensions.Logging.JsonFile.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            //var options = new ConfiguredTypesOptionsMonitor<ILoggerSink>();
+
+            var loggerSinkProvider = new LoggerSinkProvider();
+
+            loggerSinkProvider.AddOrUpdateLoggerSink(new JsonFileSink { Categories = new[] { "test" }, LogLevel = LogLevel.Debug, Name = "Unit Test Sink" });
+
+            ILogger logger = loggerSinkProvider.CreateLogger("test");
+
+            var now = DateTime.Now;
+
+            for (var i = 0; i < 1000000; ++i)
+            {
+                logger.LogDebug("Hello, file logger!");
+            }
+
+            var diff = DateTime.Now - now;
+
+            logger.LogDebug("ElapsedMilliseconds: {elapsedMilliseconds}", diff.TotalMilliseconds);
+            Debug.WriteLine($"ElapsedMilliseconds: {diff.TotalMilliseconds}");
+
+            now = DateTime.Now;
+
+            Assert.Equal(0, loggerSinkProvider.WaitOn(TimeSpan.FromSeconds(300)));
+
+            var diff2 = DateTime.Now - now;
+            var total = diff + diff2;
+
+            Debug.WriteLine($"ElapsedSeconds: {diff2.TotalSeconds} Total: {total.TotalSeconds}");
+
+            Thread.Sleep(15000);
+
+         }
+    }
+}
