696afcd2e61a1b633bf9b1cfbc0675d795448d54
André Lötzsch <andre.loetzsch@live.de>
2022-02-27 12:42:20 +0100

    File logger supports maximum file size and filename templates.

--- Tentakel.Extensions.Logging.File/src/Tentakel.Extensions.Logging.File/FileSink.cs
+++ Tentakel.Extensions.Logging.File/src/Tentakel.Extensions.Logging.File/FileSink.cs
@@ -1,99 +1,168 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 using Tentakel.Extensions.Logging.LoggerSinks;
 
 namespace Tentakel.Extensions.Logging.JsonFile
 {
     public class FileSink : LoggerSinkBase
     {
-
+        private const string defaultFileNameTemplate = "{baseDirectory}/Logging/{dateTime:yyyy}/{dateTime:MM}/{processName}/{dateTime:yyyy-MM-dd}.{processId}.log";
         private FileStream? _fileStream;
+        private DateTime _fileNameExpiryDateTime = DateTime.MinValue;
+
+        public ITextFormatter? TextFormatter { get; set; }
+
+        private string _fileNameTemplate = defaultFileNameTemplate;
+        public string FileNameTemplate
+        {
+            get => this._fileNameTemplate;
+            set
+            {
+                this._fileNameTemplate = value;
+                this._fileNameExpiryDateTime = DateTime.MinValue;
+            }
+        }
+
+        public string? FileName { get; private set; }
+        public int MaxFileSize { get; set; }
+        public bool OverrideExistingFile { get; set; }
 
         public override async void Log(LogEntry logEntry)
         {
-
-            if (this._fileStream == null)
+            if (this._fileNameExpiryDateTime <= logEntry.DateTime || this._fileStream == null)
             {
-                var path = Path.Combine(AppContext.BaseDirectory, "Logging");
+                if (this._fileStream != null) this._fileStream.Close();
 
-                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+                this.SetFileName(logEntry);
 
-                path = Path.Combine(path, "Trace.log");
-                //this._fileStream = File.Open(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+                var directory = Path.GetDirectoryName(this.FileName);
 
-                this._fileStream = System.IO.File.Open(path, FileMode.Create, FileAccess.Write, FileShare.Read);
+                if (!string.IsNullOrWhiteSpace(directory) && !Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
 
+                if ( this.OverrideExistingFile)
+                {
+                    var fileExtension = Path.GetExtension(this.FileName) ?? string.Empty;
+                    var index = 1;
+                    var fileName = this.FileName!.Replace(fileExtension, $".Part{index}{fileExtension}");
+
+                    while (File.Exists(fileName))
+                    {
+                        File.Delete(fileName);
+                        index++;
+                        fileName = this.FileName!.Replace(fileExtension, $".Part{index}{fileExtension}");
+                    }
+                }
+
+                this._fileStream = File.Open(this.FileName!, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
+
+                if (!this.OverrideExistingFile)
+                {
+                    this._fileStream.Position = this._fileStream.Length;
+                }
             }
+            
+            var logString = this.TextFormatter != null ?
+                this.TextFormatter.Format(logEntry) :
+                $"[{logEntry.LogEntryId:0000000}] [{logEntry.DateTime:yyyy.MM.dd hh:mm:ss}] [{logEntry.LogLevel}] [{logEntry.LogCategory}] {logEntry.Source} - {logEntry.Message}\r\n";
 
-            //var x = 
-            //"{\r\n" +
-            //$"  \"DateTime\": \"{logEntry.DateTime:o}\",\r\n" +
-            //$"  \"LogEntryId\": {logEntry.LogEntryId},\r\n" +
-            //$"  \"ApplicationName\": \"{logEntry.ApplicationName}\",\r\n" +
-            //$"  \"UserName\": \"{logEntry.UserName}\",\r\n" +
-            //"  \"DomainName\": \"L316IT012\",\r\n" +
-            //"  \"MachineName\": \"L316IT012\",\r\n" +
-            //"  \"AppDomainId\": 1,\r\n" +
-            //"  \"ProcessId\": 1388,\r\n" +
-            //"  \"ProcessName\": \"testhost\",\r\n" +
-            //"  \"ThreadId\": 16,\r\n" +
-            //"  \"ThreadName\": \".NET Long Running Task\",\r\n" +
-            //"  \"LoggerSinkType\": \"JsonFileSink\",\r\n" +
-            //"  \"LoggerSinkName\": \"Unit Test Sink\",\r\n" +
-            //"  \"StackTrace\": null,\r\n" +
-            //"  \"Exception\": null,\r\n" +
-            //"  \"State\": [" +
-            //"    {" +
-            //"      \"Key\": \"{OriginalFormat}\",\r\n" +
-            //"     \"Value\": \"Hello, file logger!\"" +
-            //"    }" +
-            //"  ],\r\n" +
-            //"  \"Correlation\": null,\r\n" +
-            //"  \"LogLevel\": 1,\r\n" +
-            //"  \"LogCategory\": \"\",\r\n" +
-            //"  \"EventId\": 0,\r\n" +
-            //"  \"SourceCategory\": \"test\",\r\n" +
-            //"  \"Source\": null,\r\n" +
-            //"  \"Message\": \"Hello, file logger!\",\r\n" +
-            //"  \"DateTimeFormat\": \"yyyy-MM-dd HH:mm:ss fff\",\r\n" +
-            //"  \"Attributes\": {" +
-            //"    \"{OriginalFormat}\": \"Hello, file logger!\"" +
-            //"  },\r\n" +
-            //"  \"Scopes\": null" +
-            //"}\r\n";
+            var buffer = Encoding.UTF8.GetBytes(logString);
 
+            if (this.MaxFileSize > 0 && this._fileStream.Length + buffer.Length > this.MaxFileSize)
+            {
+                var fileExtension = Path.GetExtension(this.FileName) ?? string.Empty;
+                var index = 1;
+                var newFileName = this.FileName!.Replace(fileExtension, $".Part{index}{fileExtension}");
 
-            //Thread.Sleep(10);
+                while (File.Exists(newFileName))
+                {
+                    index++;
+                    newFileName = this.FileName!.Replace(fileExtension, $".Part{index}{fileExtension}");
+                }
 
-
-            //var json = System.Text.Json.JsonSerializer.Serialize(logEntry, options: new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
-
-
-
-            var buffer = Encoding.UTF8.GetBytes($"[{logEntry.LogEntryId:0000000}] [{logEntry.DateTime:yyyy.MM.dd hh:mm:ss}] [{logEntry.LogLevel}] [{logEntry.LogCategory}] {logEntry.Source} - {logEntry.Message}\r\n");
-            //var buffer = Encoding.UTF8.GetBytes($"[{logEntry.LogEntryId:0000000}] [{logEntry.DateTime:yyyy.MM.dd hh:mm:ss}] {logEntry.Message}\r\n");
-            //var buffer = Encoding.UTF8.GetBytes($"{json},\r\n");
-
-            //var buffer = Encoding.UTF8.GetBytes(string.Concat(logEntry.ToString(), Environment.NewLine));
- 
-
-            //this._fileStream.Write(buffer, 0, buffer.Length);
-            //this._fileStream.Flush();
-
-
+                this._fileStream.Close();
+                File.Move(this.FileName, newFileName, true);
+                this._fileStream = File.Open(this.FileName!, FileMode.Create, FileAccess.Write, FileShare.Read);
+            }
 
             this._fileStream.Write(buffer, 0, buffer.Length);
             this._fileStream.Flush();
-
-
-
-
-
-
-
-
-
-
         }
+
+        private void SetFileName(LogEntry logEntry)
+        {
+            var fileName = this.FileNameTemplate;
+            fileName = fileName.Replace('\\', Path.DirectorySeparatorChar);
+            fileName = fileName.Replace('/', Path.DirectorySeparatorChar);
+
+            TimeSpan ts = TimeSpan.Zero;
+
+            foreach (var dateTimeFormat in ValuesFormatter.ExtractDateTimeFormats(fileName))
+            {
+                var dateTimeAsString = logEntry.DateTime.ToString(dateTimeFormat);
+                var dateTimeAsStringMinValue = new DateTime(1, 1, 1, 1, 1, 1).ToString(dateTimeFormat);
+
+                if (DateTime.TryParseExact(dateTimeAsStringMinValue, dateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out var dateTime))
+                {
+                    if (ts.Seconds == 0) ts = ts.Add(TimeSpan.FromSeconds(dateTime.Second));
+                    if (ts.Minutes == 0) ts = ts.Add(TimeSpan.FromMinutes(dateTime.Minute));
+                    if (ts.Hours == 0) ts = ts.Add(TimeSpan.FromHours(dateTime.Hour));
+                    if (ts.Days == 0) ts = ts.Add(TimeSpan.FromDays(dateTime.Day));
+                }
+
+                fileName = fileName.Replace(string.Concat("{dateTime:", dateTimeFormat, "}"), dateTimeAsString);
+            }
+
+            if (ts.Seconds > 0)
+            {
+                this._fileNameExpiryDateTime = logEntry.DateTime.AddSeconds(1);
+            }
+            else if (ts.Minutes > 0)
+            {
+                this._fileNameExpiryDateTime = logEntry.DateTime.AddMinutes(1);
+            }
+            else if (ts.Hours > 0)
+            {
+                this._fileNameExpiryDateTime = logEntry.DateTime.AddMinutes(1);
+            }
+            else if (ts.Days > 0)
+            {
+                this._fileNameExpiryDateTime = logEntry.DateTime.Date.AddDays(1);
+            }
+
+
+            foreach (var key in ValuesFormatter.ExtractKeys(fileName))
+            {
+                switch (key)
+                {
+                    case "baseDirectory":
+                        fileName = fileName.Replace("{baseDirectory}", AppDomain.CurrentDomain.BaseDirectory);
+                        break;
+                    case "processName":
+                        fileName = fileName.Replace("{processName}", logEntry.ProcessName);
+                        break;
+                    case "processId":
+                        fileName = fileName.Replace("{processId}", logEntry.ProcessId.ToString());
+                        break;
+                    case "appDomainId":
+                        fileName = fileName.Replace("{appDomainId}", logEntry.AppDomainId.ToString());
+                        break;
+                    case "applicationName":
+                        fileName = fileName.Replace("{applicationName}", logEntry.ApplicationName);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            this.FileName = fileName;
+        }
+
+
+
 
 
         protected override void Dispose(bool disposing)
---
+++ Tentakel.Extensions.Logging.File/src/Tentakel.Extensions.Logging.File/ValuesFormatter.cs
@@ -0,0 +1,57 @@
+﻿namespace Tentakel.Extensions.Logging.JsonFile
+{
+    public class ValuesFormatter
+    {
+
+        public ValuesFormatter(string formatTemplate)
+        {
+            this.FormatTemplate = formatTemplate;
+        }
+
+        public string FormatTemplate { get; }
+
+        public static IEnumerable<string> ExtractDateTimeFormats(string value)
+        {
+            var startIndex = value.IndexOf("{dateTime");
+
+            while (startIndex > -1)
+            {
+                var endIndex = value.IndexOf("}", startIndex);
+
+                if (endIndex > startIndex + 10)
+                {
+                    yield return value.Substring(startIndex +10, endIndex - startIndex - 10);
+                }
+
+                startIndex = value.IndexOf("{dateTime:", startIndex +1);
+            }
+        }
+
+        public static IEnumerable<string> ExtractKeys(string value)
+        {
+            var startIndex = value.IndexOf("{");
+
+            while (startIndex > -1)
+            {
+                var endIndex = value.IndexOf("}", startIndex);
+
+                if (endIndex > startIndex + 1)
+                {
+                    var subStartIndex = value.IndexOf("{", startIndex + 1);
+
+                    while (subStartIndex > -1 && subStartIndex < endIndex)
+                    {
+                        startIndex = subStartIndex;
+                        subStartIndex = value.IndexOf("{", startIndex + 2);
+                    }
+
+                    yield return value.Substring(startIndex + 1, endIndex - startIndex - 1);
+
+                }
+
+                startIndex = value.IndexOf("{", startIndex + 1);
+            }
+        }
+    }
+
+}
