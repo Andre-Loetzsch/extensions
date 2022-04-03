9264f7d308234de4548cb6c97e99192ca1d81929
André Lötzsch <andre.loetzsch@live.de>
2022-04-03 20:59:32 +0200

    Renamed namespaces to their corresponding file locations.

--- Tentakel.Extensions.Logging.File/src/Tentakel.Extensions.Logging.File/ValuesFormatter.cs
+++ Tentakel.Extensions.Logging.File/src/Tentakel.Extensions.Logging.File/ValuesFormatter.cs
@@ -1,4 +1,4 @@
-﻿namespace Tentakel.Extensions.Logging.JsonFile
+﻿namespace Tentakel.Extensions.Logging.File
 {
     public class ValuesFormatter
     {
@@ -12,44 +12,44 @@
 
         public static IEnumerable<string> ExtractDateTimeFormats(string value)
         {
-            var startIndex = value.IndexOf("{dateTime");
+            var startIndex = value.IndexOf("{dateTime", StringComparison.Ordinal);
 
             while (startIndex > -1)
             {
-                var endIndex = value.IndexOf("}", startIndex);
+                var endIndex = value.IndexOf("}", startIndex, StringComparison.Ordinal);
 
                 if (endIndex > startIndex + 10)
                 {
                     yield return value.Substring(startIndex +10, endIndex - startIndex - 10);
                 }
 
-                startIndex = value.IndexOf("{dateTime:", startIndex +1);
+                startIndex = value.IndexOf("{dateTime:", startIndex +1, StringComparison.Ordinal);
             }
         }
 
         public static IEnumerable<string> ExtractKeys(string value)
         {
-            var startIndex = value.IndexOf("{");
+            var startIndex = value.IndexOf("{", StringComparison.Ordinal);
 
             while (startIndex > -1)
             {
-                var endIndex = value.IndexOf("}", startIndex);
+                var endIndex = value.IndexOf("}", startIndex, StringComparison.Ordinal);
 
                 if (endIndex > startIndex + 1)
                 {
-                    var subStartIndex = value.IndexOf("{", startIndex + 1);
+                    var subStartIndex = value.IndexOf("{", startIndex + 1, StringComparison.Ordinal);
 
                     while (subStartIndex > -1 && subStartIndex < endIndex)
                     {
                         startIndex = subStartIndex;
-                        subStartIndex = value.IndexOf("{", startIndex + 2);
+                        subStartIndex = value.IndexOf("{", startIndex + 2, StringComparison.Ordinal);
                     }
 
                     yield return value.Substring(startIndex + 1, endIndex - startIndex - 1);
 
                 }
 
-                startIndex = value.IndexOf("{", startIndex + 1);
+                startIndex = value.IndexOf("{", startIndex + 1, StringComparison.Ordinal);
             }
         }
     }
