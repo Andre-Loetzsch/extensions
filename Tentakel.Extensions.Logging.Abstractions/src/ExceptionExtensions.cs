de98262d14ceeda2bf5d2350131b93c9fa3e25d0
Andre-Loetzsch <andre.loetzsch@live.de>
2022-05-03 15:42:20 +0200

    worked on TextFormatters

---
+++ Tentakel.Extensions.Logging.Abstractions/src/ExceptionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace Tentakel.Extensions.Logging.Abstractions;
+
+public static class ExceptionExtensions
+{
+    public static string GetAllMessages(this Exception exception)
+    {
+        var sb = new StringBuilder();
+        var ex = exception;
+
+        while (ex != null)
+        {
+            if (sb.Length > 0) sb.AppendLine();
+            sb.Append('[').Append(ex.GetType()).Append("] ").Append(ex.Message);
+            ex = ex.InnerException;
+        }
+
+        return sb.ToString();
+    }
+}
