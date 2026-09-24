f3caf4fbc1b1f025696bd80e6a132367e4d540fc
André Lötzsch <86790735+Andre-Loetzsch@users.noreply.github.com>
2026-09-24 15:03:31 +0200

    Bug #84 fixed (#85)
    * Bug #84 fixed
    
    * Tests fixed: Testing with VSTest target is no longer supported
    
    * Workflows modified
    
    * WaitOne(300) changed to WaitOne(1500)

--- _build_solution_debug.cmd
+++ _build_solution_debug.cmd
@@ -1,7 +1,10 @@
+@echo off
+
 rem dotnet build "%~dp0extensions.sln" --configuration Debug --property:VersionDevSuffix=dev --property:GeneratePackageOnBuild=false --property:versioningTask-disabled=true
 
+dotnet test --solution "%~dp0extensions.sln" --configuration Debug --ignore-exit-code 8
 
-dotnet pack "%~dp0extensions.sln" --configuration Debug --output "%~dp0packoutput" --no-restore --property:versioningTask-disabled=true --property:VersionDevSuffix=dev
+rem dotnet pack "%~dp0extensions.sln" --configuration Debug --output "%~dp0packoutput" --no-restore --property:versioningTask-disabled=true --property:VersionDevSuffix=dev
 pause
 
 
